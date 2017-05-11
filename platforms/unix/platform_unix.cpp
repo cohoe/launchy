99b52cb881e703e2ea44617b8a9cd7e6a3a48a87
Grant Cohoe <grant@grantcohoe.com>
2017-05-10 23:42:44 -0400

    Fix for problem reading desktop files with multiple name entries (stream009)

--- platforms/unix/platform_unix.cpp
+++ platforms/unix/platform_unix.cpp
@@ -158,19 +158,32 @@ void PlatformUnix::alterItem(CatItem* item) {
     QString icon = "";
     QString exe = "";
     while(!file.atEnd()) {
-	QString line = QString::fromUtf8(file.readLine());
-	
-	if (line.startsWith("Name[" + locale, Qt::CaseInsensitive)) 
-	    name = line.split("=")[1].trimmed();
-	
+	QString line = file.readLine();
 
-	else if (line.startsWith("Name=", Qt::CaseInsensitive)) 
-	    name = line.split("=")[1].trimmed();
+        if (name == "") {
+            if (line.startsWith("Name[" + locale, Qt::CaseInsensitive)) {
+                name = line.split("=")[1].trimmed();
+                continue;
+            }
+            else if (line.startsWith("Name=", Qt::CaseInsensitive)) {
+                name = line.split("=")[1].trimmed();
+                continue;
+            }
+        }
 
-	else if (line.startsWith("Icon", Qt::CaseInsensitive))
-	    icon = line.split("=")[1].trimmed();
-	else if (line.startsWith("Exec", Qt::CaseInsensitive))
-	    exe = line.split("=")[1].trimmed();	
+        if (icon == "") {
+            if (line.startsWith("Icon", Qt::CaseInsensitive)) {
+                icon = line.split("=")[1].trimmed();
+                continue;
+            }
+        }
+
+        if (exe == "") {
+	    if (line.startsWith("Exec", Qt::CaseInsensitive)) {
+                exe = line.section("=", 1).trimmed();
+                continue;
+            }
+        }
     }
     if (name.size() >= item->shortName.size() - 8) {
 	item->shortName = name;
